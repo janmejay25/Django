c394516ba3175734228a2a6642e578d7cddbc23b
janmejay25 <janmjaypandya@gmail.com>
2025-03-18 21:32:48 +0530

    print

--- print.py
+++ print.py
@@ -1,32 +1,32 @@
-# print("hello world")
-# #seperator
-# print("hello", "world", sep="***")
-# #restricted string
-# print(r"hello\nworld")
-# #path read using double backslash
-# print("c:\\newfolder\\test.txt")
-# #using r
-# # print("C:\Users\ASUS\OneDrive\Desktop\PyConf\django_socet")
-# #type casting
-# a=10
-# x=float(a)
-# print(x)
-# print(type(x))
+print("hello world")
+#seperator
+print("hello", "world", sep="***")
+#restricted string
+print(r"hello\nworld")
+#path read using double backslash
+print("c:\\newfolder\\test.txt")
+#using r
+# print("C:\Users\ASUS\OneDrive\Desktop\PyConf\django_socet")
+#type casting
+a=10
+x=float(a)
+print(x)
+print(type(x))
 
-# list = [1,2,3,4,5,True]
-# list[-1]=False
-# print(list)
+list = [1,2,3,4,5,True]
+list[-1]=False
+print(list)
 
 
-# angle1 = int(input("Enter the first angle: "))
-# angle2 = int(input("Enter the second angle: "))
-# angle3 = int(input("Enter the third angle: "))
-# if angle1 == angle2 == angle3:
-#     print("Equilateral Triangle")
-# elif angle1 == angle2 or angle2 == angle3 or angle3 == angle1:
-#     print("Isosceles Triangle")
-# else:   
-#     print("Scalene Triangle")
+angle1 = int(input("Enter the first angle: "))
+angle2 = int(input("Enter the second angle: "))
+angle3 = int(input("Enter the third angle: "))
+if angle1 == angle2 == angle3:
+    print("Equilateral Triangle")
+elif angle1 == angle2 or angle2 == angle3 or angle3 == angle1:
+    print("Isosceles Triangle")
+else:   
+    print("Scalene Triangle")
 
 # for loop z
 for row in range(5):
